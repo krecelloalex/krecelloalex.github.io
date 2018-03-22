d3bb0fcd07b95ded4cfceee5a72555477f90d666
krecelloalex <34172272+krecelloalex@users.noreply.github.com>
2018-03-22 11:50:30 +0200

    update js
    fixed crolling slow

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
-$(document).ready(function(){
-    $('.go_to').click( function(){ // ловим клик по ссылке с классом go_to
-	var scroll_el = $(this).attr('href'); // возьмем содержимое атрибута href, должен быть селектором, т.е. например начинаться с # или .
-        if ($(scroll_el).length != 0) { // проверим существование элемента чтобы избежать ошибки
-	    $('html, body').animate({ scrollTop: $(scroll_el).offset().top }, 500); // анимируем скроолинг к элементу scroll_el
-        }
-	    return false; // выключаем стандартное действие
-    });
+$(document).ready(function() {
+  $("a.scrollto").click(function() {
+    var elementClick = $(this).attr("href")
+    var destination = $(elementClick).offset().top;
+    jQuery("html:not(:animated),body:not(:animated)").animate({
+      scrollTop: destination
+    }, 800);
+    return false;
+  });
 });
